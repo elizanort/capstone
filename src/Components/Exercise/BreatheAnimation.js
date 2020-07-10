a9522223a8209317eafdde3eb8b8fa50a1e40f5f
“garrettruble1” <garrettruble1@gmail.com>
2020-07-10 14:03:11 -0400

    Commit and push to show Tyler most up to date code

--- src/Components/Exercise/BreatheAnimation.js
+++ src/Components/Exercise/BreatheAnimation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ActivityTimer from './Timer.js';
 
 class BreatheAnimation extends Component {
 
@@ -9,6 +10,7 @@ class BreatheAnimation extends Component {
                 <div className="animationContainer">
                     <div className="circle"></div>
                     <h2 className="animationText">Breathe With The Circle</h2>
+                    {/* <ActivityTimer /> */}
                 </div>
             </div>
         )

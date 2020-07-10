b94da050e55a96b6ca39aaa5443adae30a235818
“elizanort” <elizabethnortman@gmail.com>
2020-07-10 14:02:03 -0400

    added

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Exercise extends Component {
-    render() {
-        return (
-            <div className="activity-container">
-                <div className="card">
-                    {/* Activity logic */}
-                </div>
+  render() {
+    const NavWrap = styled.div`
+      padding: 1rem;
+      display: flex;
+    `;
+    return (
+      <div className="activity-container">
+          <NavWrap>
+            <i class="fas fa-times"></i>
+          </NavWrap>
+        <div className="card">{/* Activity logic */}</div>
 
-                <div>
-                    <button>
-                        {/* timer function */}
-                    </button>
-                </div>
-                
-            </div>
-        )
-    }
+        <div>
+          <button>{/* timer function */}</button>
+        </div>
+      </div>
+    );
+  }
 }

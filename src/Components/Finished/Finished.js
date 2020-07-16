6d9c00b400a3966a8ca4d4e877a26cae6d14fd07
“garrettruble1” <garrettruble1@gmail.com>
2020-07-16 19:58:06 -0400

    Tying off some styling and functionality

--- src/Components/Finished/Finished.js
+++ src/Components/Finished/Finished.js
@@ -1,35 +1,41 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const screenOne = (
-  <React.Fragment>
-    <h1>THIS IS</h1>
-  </React.Fragment>
-);
-
-const screenTwo = (
-  <React.Fragment>
-    <h1>AAAAA</h1>
-  </React.Fragment>
-);
-
-const screenThree = (
-  <React.Fragment>
-    <h1>TEST</h1>
-  </React.Fragment>
-);
-
-export default class Finished extends Component {
-  whichScreen(activityName) {
-    if (activityName === "activity1") {
-      return screenOne;
-    } else if (activityName === "activity2") {
-      return screenTwo;
-    } else {
-      return screenThree;
-    }
-  }
+class Finished extends Component {
 
   render() {
-    return <>{this.whichScreen()}</>;
+
+    const Header = styled.p`
+      font-family: "Raleway";
+      margin-top: 3rem;
+      font-size: 2rem;
+      text-align: center;
+      color: #006d77;
+    `;
+
+    const FinishButton = styled.button`
+      background: #006d77;
+      border-radius: 12rem;
+      color: #e6f2f5;
+      text-align: center;
+      width: 12rem;
+      margin: 2rem;
+      padding: 0.6rem 0rem;
+      box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.1);
+    `;
+
+    return (
+      <div className="screenContainer">
+        <div className="logo"></div>
+        <div className="finishContainer">
+          <Header>Ah, that's better!</Header>
+          <div className="finishGraphic"></div>
+          <Link to='/'><button><FinishButton>FINISH</FinishButton></button></Link>
+        </div>
+      </div>
+    );
   }
 }
+
+export default Finished;
